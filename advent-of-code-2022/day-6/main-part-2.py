b42f5cdca60a7c745acb673fc6c2da84244ac4ef
Marco Caldera <marco.caldera93@icloud.com>
2022-12-10 21:56:09 +0100

    feat: add day6

---
+++ advent-of-code-2022/day-6/main-part-2.py
@@ -0,0 +1,20 @@
+# https://adventofcode.com/2022/day/6
+
+result = 0
+
+with open('input.txt', mode='r') as f:
+    file_content = f.readlines()[0]
+
+    start_of_packet = []
+    for idx, char in enumerate(file_content):
+        start_of_packet.append(char)
+        unique_set_of_values = list(set(start_of_packet))
+
+        if len(start_of_packet) == len(unique_set_of_values):
+            if len(unique_set_of_values) == 14:
+                result = idx + 1
+                break
+        else:
+            start_of_packet = start_of_packet[1:]
+
+print(result)
