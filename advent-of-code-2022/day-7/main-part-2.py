bfd8cf5439cfa0a0319b3d79b174498785680559
Marco Caldera <marco.caldera93@icloud.com>
2022-12-11 15:15:33 +0100

    feat: add day7 part2

---
+++ advent-of-code-2022/day-7/main-part-2.py
@@ -0,0 +1,77 @@
+# https://adventofcode.com/2022/day/7
+
+directories = {}
+visited_paths = {}
+current_path = []
+
+def is_bash_command(instruction):
+  return instruction.startswith('$')
+
+def is_change_directory(instruction):
+  return instruction.startswith('cd')
+
+def is_list_directories(instruction):
+  return instruction.startswith('ls')
+
+def is_go_to_root_directory(instruction):
+  return instruction.startswith('/')
+
+def is_go_to_previous_directory(instruction):
+  return instruction.startswith('..')
+
+def is_showing_directory(instruction):
+  return instruction.startswith('dir')
+
+with open('input.txt', mode='r') as f:
+    file_content = f.readlines()
+
+    for instruction in file_content:
+        instruction = instruction.replace('\n', '')
+
+        if is_bash_command(instruction):
+            instruction = instruction.replace("$ ", "")
+
+            if is_change_directory(instruction):
+                instruction = instruction.replace("cd ", "")
+
+                if is_go_to_previous_directory(instruction):
+                    current_path.pop()
+                else:
+                    current_path.append(instruction)
+            
+            elif is_list_directories(instruction):
+                continue     
+        
+        else: # Is result of ls command
+            if not is_showing_directory(instruction):
+                file_size, file_name = instruction.split(" ")
+                file_size = int(file_size)
+
+                # Update folder size up through the current_path
+                # Example: current_path: ['/', 'd']
+                # iteration-1: update size of: path = '/'
+                # iteration-2: update size of: path = '//d'
+
+                for idx in range(len(current_path)):
+                    path = "/".join(current_path[:idx + 1])
+
+                    if path in directories:
+                        directories[path] += file_size
+                    else:
+                        directories[path] = file_size
+
+
+file_system_size = 70_000_000
+file_system_size_used = directories["/"]
+file_system_size_unused = file_system_size - file_system_size_used
+
+required_free_space = 30_000_000
+space_to_free = required_free_space - file_system_size_unused
+
+dir_to_delete_size = 0
+
+for directory in directories:
+    if directories[directory] >= space_to_free:
+        dir_to_delete_size = directories[directory]
+
+print(dir_to_delete_size)
